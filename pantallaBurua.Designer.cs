1c32502f2949b347ba569975658625d5731ee239
Ander Criado Teijeira <and_cri_tei@goierrieskola.org>
2026-04-19 20:27:54 +0200

    langile buruaren pantalla eginda

---
+++ pantallaBurua.Designer.cs
@@ -0,0 +1,413 @@
+﻿namespace erronka3_1T
+{
+    partial class pantallaBurua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button10 = new Button();
+            button5 = new Button();
+            groupBox1 = new GroupBox();
+            textBox9 = new TextBox();
+            izena = new CheckedListBox();
+            label10 = new Label();
+            button7 = new Button();
+            label18 = new Label();
+            textBox18 = new TextBox();
+            button4 = new Button();
+            button3 = new Button();
+            b_ezab_btn = new Button();
+            bezero_id = new TextBox();
+            label9 = new Label();
+            button1 = new Button();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button10
+            // 
+            button10.Location = new Point(163, 328);
+            button10.Name = "button10";
+            button10.Size = new Size(293, 29);
+            button10.TabIndex = 104;
+            button10.Text = "Hornitzaile eta langilearen erlazioak ikusi";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(459, 328);
+            button5.Name = "button5";
+            button5.Size = new Size(106, 29);
+            button5.TabIndex = 103;
+            button5.Text = "Zitak ikusi";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(izena);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(label18);
+            groupBox1.Controls.Add(textBox18);
+            groupBox1.Location = new Point(20, 369);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(519, 195);
+            groupBox1.TabIndex = 102;
+            groupBox1.TabStop = false;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(296, 61);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(125, 27);
+            textBox9.TabIndex = 80;
+            // 
+            // izena
+            // 
+            izena.FormattingEnabled = true;
+            izena.Items.AddRange(new object[] { "izena", "abizena", "pasahitza", "email", "NAN", "telefonoa", "helbidea", "suskripzioa" });
+            izena.Location = new Point(88, 29);
+            izena.Name = "izena";
+            izena.Size = new Size(177, 158);
+            izena.TabIndex = 74;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(317, 38);
+            label10.Name = "label10";
+            label10.Size = new Size(75, 20);
+            label10.TabIndex = 79;
+            label10.Text = "Bezero id:";
+            // 
+            // button7
+            // 
+            button7.Location = new Point(284, 142);
+            button7.Name = "button7";
+            button7.Size = new Size(147, 29);
+            button7.TabIndex = 77;
+            button7.Text = "Aldatu";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(275, 88);
+            label18.Name = "label18";
+            label18.Size = new Size(164, 20);
+            label18.TabIndex = 75;
+            label18.Text = "Bezeroaren datu berria:";
+            // 
+            // textBox18
+            // 
+            textBox18.Location = new Point(296, 110);
+            textBox18.Name = "textBox18";
+            textBox18.Size = new Size(125, 27);
+            textBox18.TabIndex = 76;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(401, 294);
+            button4.Name = "button4";
+            button4.Size = new Size(164, 29);
+            button4.TabIndex = 101;
+            button4.Text = "Langileak ikusi";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(213, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(178, 29);
+            button3.TabIndex = 100;
+            button3.Text = "Bezeroak ikusi";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // b_ezab_btn
+            // 
+            b_ezab_btn.Location = new Point(10, 326);
+            b_ezab_btn.Name = "b_ezab_btn";
+            b_ezab_btn.Size = new Size(147, 29);
+            b_ezab_btn.TabIndex = 99;
+            b_ezab_btn.Text = "Bezeroa ezabatu";
+            b_ezab_btn.UseVisualStyleBackColor = true;
+            // 
+            // bezero_id
+            // 
+            bezero_id.Location = new Point(79, 293);
+            bezero_id.Name = "bezero_id";
+            bezero_id.Size = new Size(125, 27);
+            bezero_id.TabIndex = 98;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(7, 296);
+            label9.Name = "label9";
+            label9.Size = new Size(75, 20);
+            label9.TabIndex = 97;
+            label9.Text = "Bezero id:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(638, 536);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 29);
+            button1.TabIndex = 96;
+            button1.Text = "Gehitu bezeroa";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(724, 503);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 27);
+            textBox8.TabIndex = 95;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(724, 473);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 94;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(724, 443);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 93;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(724, 413);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 92;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(724, 383);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 91;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(724, 354);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 90;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(724, 325);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 89;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(724, 295);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 88;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(573, 506);
+            label8.Name = "label8";
+            label8.Size = new Size(135, 20);
+            label8.TabIndex = 87;
+            label8.Text = "Bezero suskripzioa:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(572, 479);
+            label7.Name = "label7";
+            label7.Size = new Size(120, 20);
+            label7.TabIndex = 86;
+            label7.Text = "Bezero helbidea:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(572, 447);
+            label6.Name = "label6";
+            label6.Size = new Size(126, 20);
+            label6.TabIndex = 85;
+            label6.Text = "Bezero telefonoa:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(571, 415);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 20);
+            label5.TabIndex = 84;
+            label5.Text = "Bezero NAN:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(571, 384);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 20);
+            label4.TabIndex = 83;
+            label4.Text = "Bezero email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(571, 354);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 20);
+            label3.TabIndex = 82;
+            label3.Text = "Bezero pasahitza:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(571, 328);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 20);
+            label2.TabIndex = 81;
+            label2.Text = "Bezero abizena:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(571, 302);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 20);
+            label1.TabIndex = 80;
+            label1.Text = "Bezero izena:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 8);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(863, 275);
+            dataGridView1.TabIndex = 79;
+            // 
+            // pantallaBurua
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(887, 572);
+            Controls.Add(button10);
+            Controls.Add(button5);
+            Controls.Add(groupBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(b_ezab_btn);
+            Controls.Add(bezero_id);
+            Controls.Add(label9);
+            Controls.Add(button1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "pantallaBurua";
+            Text = "pantallaBurua";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button10;
+        private Button button5;
+        private GroupBox groupBox1;
+        private TextBox textBox9;
+        private CheckedListBox izena;
+        private Label label10;
+        private Button button7;
+        private Label label18;
+        private TextBox textBox18;
+        private Button button4;
+        private Button button3;
+        private Button b_ezab_btn;
+        private TextBox bezero_id;
+        private Label label9;
+        private Button button1;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView1;
+    }
+}
