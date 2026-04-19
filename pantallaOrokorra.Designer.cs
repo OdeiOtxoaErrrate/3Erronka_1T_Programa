1d36e32afb2b6dc733da86c71a635e8c2bb4e9f4
OdeiOtxoaErrrarte <ode_otx_flo@goierrieskola.org>
2026-04-19 21:24:04 +0200

    pantalla orokorran aldatu egoera botoiaren deia egiteko kodigoa eginda.

--- pantallaOrokorra.Designer.cs
+++ pantallaOrokorra.Designer.cs
@@ -54,6 +54,7 @@
             button2.TabIndex = 11;
             button2.Text = "Aldatu egoera";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label2
             // 
