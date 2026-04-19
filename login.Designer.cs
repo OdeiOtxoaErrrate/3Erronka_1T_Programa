a989284f79da7c91cdbb31e541041f79f0399e8d
Ander Criado Teijeira <and_cri_tei@goierrieskola.org>
2026-04-19 20:30:55 +0200

    login pantalla eginda

---
+++ login.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace erronka3_1T
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(92, 136);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 31;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(92, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 30;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(113, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 29;
+            label2.Text = "Pasahitza:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(103, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 20);
+            label1.TabIndex = 28;
+            label1.Text = "Erabiltzailea:";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(79, 177);
+            button3.Name = "button3";
+            button3.Size = new Size(147, 29);
+            button3.TabIndex = 27;
+            button3.Text = "Sartu";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(309, 281);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Name = "login";
+            Text = "login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private Button button3;
+    }
+}
