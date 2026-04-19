ed23ea37c45eb442cf684e55a3eaed1f836883f7
Ander Criado Teijeira <and_cri_tei@goierrieskola.org>
2026-04-19 20:29:29 +0200

    langile orokorraren pantalla eginda

---
+++ pantallaOrokorra.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace erronka3_1T
+{
+    partial class pantallaOrokorra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            label2 = new Label();
+            zita_id = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(13, 26);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(402, 218);
+            dataGridView1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(188, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 29);
+            button2.TabIndex = 11;
+            button2.Text = "Aldatu egoera";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 286);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 10;
+            // 
+            // zita_id
+            // 
+            zita_id.Location = new Point(263, 250);
+            zita_id.Name = "zita_id";
+            zita_id.Size = new Size(125, 27);
+            zita_id.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(155, 257);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Zitaren ID-a:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 277);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Zitak ikusi";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pantallaOrokorra
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(429, 339);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(zita_id);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "pantallaOrokorra";
+            Text = "pantallaOrokorra";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Label label2;
+        private TextBox zita_id;
+        private Label label1;
+        private Button button1;
+    }
+}
