4f767fc93b0c31f73c846bdfd8f1fe50e1c79a91
Manex287 <man_ola_de@goierrieskola.org>
2026-04-21 23:23:52 +0200

    Bezeroak ezabatzeko metodoa gehituta

--- langileBurua.cs
+++ langileBurua.cs
@@ -116,7 +116,104 @@ namespace erronka3_1T
             }
 
         }
+        public static DataTable hornitzaile_eta_langilearen_erlazioak_ikusi()
 
+        {
+
+            string connectionString = "Server=192.168.115.167;Database=hirugarrenerronka;Uid=erronka3;Pwd=1MG32025;";
+
+
+            string query = "SELECT * FROM erlazioa_langileak_hornitzaileak";
+
+
+            using (MySqlConnection conexion = new MySqlConnection(connectionString))
+
+            {
+
+                try
+                {
+
+                    conexion.Open();
+
+
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(query, conexion);
+
+
+                    DataTable tabla = new DataTable();
+
+
+                    adaptador.Fill(tabla);
+
+
+                    return tabla;
+
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                    MessageBox.Show("Error al cargar las citas: " + ex.Message);
+
+                    return null;
+
+                }
+
+            }
+
+        }
+        public static void bezeroak_ezabatu(string id)
+
+        {
+
+            string connectionString = "Server=192.168.115.167;Database=hirugarrenerronka;Uid=erronka3;Pwd=1MG32025;";
+
+            string query = "DELETE FROM bezeroak WHERE id = @id";
+
+            using (MySqlConnection conexion = new MySqlConnection(connectionString))
+
+            {
+
+                try
+                {
+
+                    conexion.Open();
+
+                    MySqlCommand comando = new MySqlCommand(query, conexion);
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+
+                    {
+
+                        MessageBox.Show("Bezeroa behar bezala ezabatu da.");
+
+                    }
+
+                    else
+                    {
+
+                        MessageBox.Show("Ez da aurkitu ID hori duen erregistrorik.");
+
+                    }
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                    MessageBox.Show("Errorea bezeroa ezabatzeko: " + ex.Message);
+
+                }
+
+            }
+
+        }
     }
 
 }
