8ad4112bed4d1ad3ff2869ff127575839c57576e
Ander Criado Teijeira <and_cri_tei@goierrieskola.org>
2026-04-21 23:59:37 +0200

    langilearen datuak ardatzeko metodoa eginda

--- admin.cs
+++ admin.cs
@@ -39,6 +39,69 @@ namespace erronka3_iT
                 }
             }
         }
+        public static void langileak_gehitu(string izena, string abizena, string pasahitza, string email, string postua, string telefonoa, string helbidea, string erabiltzailea)
+        {
 
+
+            string connectionString = "Server=192.168.115.167;Database=hirugarrenerronka;Uid=erronka3;Pwd=1MG32025;";
+            string query = "INSERT INTO langileak (izena, abizena, postua, helbidea, email, telefonoa, pasahitza, erabiltzailea) VALUES (@izena, @abizena, @postua, @helbidea, @email, @telefonoa, @pasahitza,@erabiltzailea)";
+            using (MySqlConnection conexion = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conexion.Open();
+                    MySqlCommand comando = new MySqlCommand(query, conexion);
+                    comando.Parameters.AddWithValue("@izena", izena);
+                    comando.Parameters.AddWithValue("@abizena", abizena);
+                    comando.Parameters.AddWithValue("@postua", postua);
+                    comando.Parameters.AddWithValue("@helbidea", helbidea);
+                    comando.Parameters.AddWithValue("@email", email);
+                    comando.Parameters.AddWithValue("@telefonoa", telefonoa);
+                    comando.Parameters.AddWithValue("@pasahitza", pasahitza);
+                    comando.Parameters.AddWithValue("@erabiltzailea", erabiltzailea);
+                    int filasAfectadas = comando.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Langilea behar bezala gehitu da.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Errorea langilea gehitzean.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errorea langilea gehitzean: " + ex.Message);
+                }
+            }
+        }
+        public static void langile_datua_aldatu(string id, string eremua, string datuBerria)
+        {
+            string connectionString = "Server=192.168.115.167;Database=hirugarrenerronka;Uid=erronka3;Pwd=1MG32025;";
+            string query = $"UPDATE langileak SET {eremua} = @datuBerria WHERE id = @id";
+            using (MySqlConnection conexion = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conexion.Open();
+                    MySqlCommand comando = new MySqlCommand(query, conexion);
+                    comando.Parameters.AddWithValue("@datuBerria", datuBerria);
+                    comando.Parameters.AddWithValue("@id", id);
+                    int filasAfectadas = comando.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Langilearen datua behar bezala aldatu da.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ez da aurkitu ID hori duen erregistrorik.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errorea langilearen datua aldatzean: " + ex.Message);
+                }
+            }
+        }
     }
 }
